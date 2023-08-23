83df8b0a8ccf06106910094c591f846a5f14d3ba
bharathrames <bharath91505@gmail.com>
2023-08-23 07:24:56 +0530

    intial

---
+++ src/components/navbar.js
@@ -0,0 +1,27 @@
+import React from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { useCookies } from "react-cookie";
+
+export const Navbar = () => {
+  const [cookies, setCookies] = useCookies(["access_token"]);
+  const navigate = useNavigate();
+
+  const logout = () => {
+    setCookies("access_token", "");
+    window.localStorage.clear();
+    navigate("/auth");
+  };
+  return (
+    <nav className="navbar">
+      <div className="nav-item effect"><Link to="/">Home</Link></div>
+      <div className="nav-item effect"><Link to="/create-recipe">Create Recipe</Link></div>
+      <div className="nav-item effect"><Link to="/saved-recipes">Saved Recipes</Link></div>
+      {!cookies.access_token ? (
+        <div class="nav-item 3d-effect"><Link to="/auth">Login/Register</Link></div>
+      ) : (
+        <button onClick={logout}> Logout </button>
+      )}
+    </nav>
+  );
+};
+
